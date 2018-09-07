b6b14d36e6a0279ac07bd70089a06d1b417438fb
Winayaka Ruhur <winayakarsp@gmail.com>
2018-09-07 14:47:35 +0700

    initial commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,26 @@
+public class Mahasiswa{
+    public String nama;
+    public String NPM;
+
+    public Mahasiswa(String nama, String NPM){
+        this.nama = nama;
+        this.NPM = NPM
+    }
+
+    public  Sting getNama(){
+        return nama;
+    }
+
+    public void setNama(Sting nama){
+        this.nama = nama;
+    }
+
+    public Sting getNPM(){
+        return NPM;
+    }
+
+    public void setNPM(Sting NPM){
+        this.NPM = NPM;
+    }
+
+} 
